c8fde95761ab15d5b95ba90c3b77c924c4508793
CJ Harries <cj@wizardsoftheweb.pro>
2019-07-05 13:01:15 -0500

    Add metrics

--- control-server/server.go
+++ control-server/server.go
@@ -1,9 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/zsais/go-gin-prometheus"
+)
 
 func main() {
 	r := gin.Default()
+	p := ginprometheus.NewPrometheus("gin")
+	p.Use(r)
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
