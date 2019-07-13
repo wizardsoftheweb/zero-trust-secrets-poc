048c1c5ef3246eb527187d80192ab05244afce9b
CJ Harries <cj@wizardsoftheweb.pro>
2019-07-13 01:54:17 -0500

    Build basic TF

--- terraform-provider-zts/main.tf
+++ terraform-provider-zts/main.tf
@@ -1,5 +1,16 @@
-provider "zts" {}
+provider "zts" {
+  etcd_host = "http://127.0.0.1:2379/"
+}
 
-resource "zts_server" "my-server" {
-  address = "1.2.3.4"
+resource "zts_secrets" "test" {
+  etcd_key = "/test-client/secrets.json"
+}
+
+resource "zts_gpg_keys" "test" {
+  directory = "."
+  batch {
+    name    = "CJ Harries"
+    email   = "cj@wotw.pro"
+    comment = "TF Zero Trust Secrets"
+  }
 }
