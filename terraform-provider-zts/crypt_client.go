381f5d9e920d0db4dcb4c7c47e2d2bfd6cccf9c2
CJ Harries <cj@wizardsoftheweb.pro>
2019-07-13 02:13:24 -0500

    Add secret generation method

--- terraform-provider-zts/crypt_client.go
+++ terraform-provider-zts/crypt_client.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/xordataexchange/crypt/config"
@@ -21,4 +25,38 @@ func newConfigManager(hosts []string, pubKeyPath string) *config.ConfigManager {
 		log.Fatal(err)
 	}
 	return &configManager
+}
+
+type RandoRequest struct {
+	Count   int      `json:"count"`
+	KvHosts []string `json:"kv_hosts"`
+	KvKey   string   `json:"kv_key"`
+	PubKey  string   `json:"pub_key"`
+}
+
+type RandoResponse struct {
+	Secrets []string `json:"message"`
+}
+
+func GenerateSecrets(controlServerUrl string, requestBody *RandoRequest) []string {
+	log.Printf("%#v", requestBody)
+	buffer := new(bytes.Buffer)
+	_ = json.NewEncoder(buffer).Encode(requestBody)
+	request, _ := http.NewRequest("POST", controlServerUrl, buffer)
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if nil != err {
+		log.Println("Unable to post request")
+		log.Fatal(err)
+	}
+	responseBody, err := ioutil.ReadAll(response.Body)
+	if nil != err {
+		log.Fatal(err)
+	}
+	var parsedResponse RandoResponse
+	err = json.Unmarshal(responseBody, &parsedResponse)
+	if nil != err {
+		log.Fatal(err)
+	}
+	return parsedResponse.Secrets
 }
