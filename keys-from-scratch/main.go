300874da9c29ea1c689b1c77e3c263ee87fe1669
CJ Harries <cj@wizardsoftheweb.pro>
2019-07-14 03:49:47 -0500

    Add test generator

--- keys-from-scratch/main.go
+++ keys-from-scratch/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"golang.org/x/crypto/openpgp"
@@ -9,18 +11,21 @@ import (
 )
 
 const (
-	name           = "CJ Harries"
-	email          = "cj@wotw.pro"
-	comment        = "Home Brew ZTS PoC"
-	minimumRsaBits = 4096
+	name              = "CJ Harries"
+	email             = "cj@wotw.pro"
+	comment           = "Home Brew ZTS PoC"
+	minimumRsaBits    = 4096
+	baseCipher        = packet.CipherAES256
+	commonCompression = packet.CompressionZLIB
+	baseLevel         = packet.BestSpeed
 )
 
 func newGenericConfig() *packet.Config {
 	return &packet.Config{
-		DefaultCipher:          packet.CipherAES256,
-		DefaultCompressionAlgo: packet.CompressionZLIB,
+		DefaultCipher:          baseCipher,
+		DefaultCompressionAlgo: commonCompression,
 		CompressionConfig: &packet.CompressionConfig{
-			Level: packet.BestSpeed,
+			Level: baseLevel,
 		},
 		RSABits: minimumRsaBits,
 	}
@@ -38,6 +43,56 @@ func generateEntity() error {
 	return nil
 }
 
+var (
+	possibleCiphersForBenchmarks = []string{
+		"packet.CipherAES128",
+		"packet.CipherAES192",
+		"packet.CipherAES256",
+	}
+	possibleCompressionAlgosForBenchmarks = []string{
+		"packet.CompressionZLIB",
+		"packet.CompressionZIP",
+		"packet.CompressionNone",
+	}
+	possibleCompressionLevelsForBenchmarks = []string{
+		"packet.BestSpeed",
+		"packet.BestCompression",
+		"packet.DefaultCompression",
+		"packet.NoCompression",
+	}
+	possibleRsaBitsForBenchmarks = []string{"2048", "4096"}
+)
+
+func draftBenchmarks() {
+	for _, cipher := range possibleCiphersForBenchmarks {
+		for _, compressionAlgo := range possibleCompressionAlgosForBenchmarks {
+			for _, level := range possibleCompressionLevelsForBenchmarks {
+				for _, rsa := range possibleRsaBitsForBenchmarks {
+					cipherName := strings.TrimPrefix(cipher, "packet.Cipher")
+					compressionAlgoName := strings.TrimPrefix(compressionAlgo, "packet.Compression")
+					levelName := strings.TrimPrefix(level, "packet.")
+					fmt.Printf(
+						`
+func (s *CreationSuite) BenchmarkCreationOf%sx%sx%sx%s(c *C) {
+	benchmarkSingleConfig(%s, %s, %s, %s, c)
+}
+`,
+						cipherName,
+						compressionAlgoName,
+						levelName,
+						rsa,
+						cipher,
+						compressionAlgo,
+						level,
+						rsa,
+					)
+				}
+			}
+		}
+	}
+}
+
 func main() {
-	_ = generateEntity()
+	//_ = generateEntity()
+	draftBenchmarks()
 }
